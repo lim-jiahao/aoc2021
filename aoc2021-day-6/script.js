569e6aaa45a81a99f469038ca843471eb4a69199
lim-jiahao <lim.jiahaoo@gmail.com>
2021-12-12 00:18:32 +0800

    part 2

--- aoc2021-day-6/script.js
+++ aoc2021-day-6/script.js
@@ -1,28 +1,22 @@
 const fs = require("fs");
 
-let fishTimers = fs.readFileSync("input.txt", "utf8").split(",").map((el) => Number(el));
+const fishTimers = fs.readFileSync("input.txt", "utf8").split(",").map((el) => Number(el));
 
-const NUM_DAYS = 80;
+const NUM_DAYS = 256;
 
-const passOneDay = (arr) => {
-  let fishToAdd = 0;
-  arr = arr.map((timer) => {
-    if (timer === 0) {
-      timer = 6;
-      fishToAdd += 1;
-    } else timer -= 1;
+// 9 elements -> index 0 to 8
+const timerCounts = new Array(9).fill(0);
 
-    return timer;
-  });
-
-  for (let i = 0; i < fishToAdd; i += 1) {
-    arr.push(8);
-  }
-  return arr;
-}
+fishTimers.forEach((timer) => timerCounts[timer] += 1);
 
 for (let i = 0; i < NUM_DAYS; i += 1) {
-  fishTimers = passOneDay(fishTimers);
+  const fishTimerZero = timerCounts[0];
+
+  for (let j = 0; j < timerCounts.length; j += 1) {
+    timerCounts[j] = timerCounts[j + 1];
+  }
+  timerCounts[6] += fishTimerZero;
+  timerCounts[8] = fishTimerZero;
 }
 
-console.log(fishTimers.length);
+console.log(timerCounts.reduce((acc, cur) => acc + cur));
