f4273d9c0adc94b5306b4016cac92745f61c9e30
lim-jiahao <lim.jiahaoo@gmail.com>
2021-12-14 16:24:09 +0800

    part 2

--- aoc2021-day-10/script.js
+++ aoc2021-day-10/script.js
@@ -7,35 +7,47 @@ const mappings = {
   ']': '[',
   '}': '{',
   '>': '<',
+  '(': ')',
+  '[': ']',
+  '{': '}',
+  '<': '>',
 }
 
 const points = {
-  ')': 3,
-  ']': 57,
-  '}': 1197,
-  '>': 25137,
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4,
 }
 
 const openings = ['(', '[', '{', '<'];
-const illegalChars = [];
+const scores = [];
 
 lines.forEach((line) => {
   const openingArray = [];
 
+  let isCorrupt = false;
   const chars = line.split('');
   for (let i = 0; i < chars.length; i += 1) {
     const char = chars[i];
     if (openings.includes(char)) openingArray.push(char);
     else {
       const rightOpening = mappings[char];
-      if (openingArray[openingArray.length - 1] === rightOpening) openingArray.pop();
-      else {
-        illegalChars.push(char);
+      if (openingArray[openingArray.length - 1] !== rightOpening) {
+        isCorrupt = true;
         break;
+      } else {
+        openingArray.pop();
       }
     }
   }
+
+  if (!isCorrupt) {
+    const correctClosings = openingArray.map((el) => mappings[el]).reverse();
+    console.log(correctClosings);
+    scores.push(correctClosings.reduce((acc, cur) => acc * 5 + points[cur], 0));
+  }
 }) 
 
-const total = illegalChars.reduce((acc, cur) => acc + points[cur], 0);
-console.log(total);
+scores.sort((a, b) => a - b);
+console.log(scores[(scores.length - 1) / 2]);
